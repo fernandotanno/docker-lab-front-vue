973e88734ea5e11df0850b5c6a1fea368ace900b
fernandotanno <fernando.tanno@gmail.com>
2020-01-31 23:23:59 -0300

    Alter Bearer

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -12,7 +12,7 @@ Vue.use({
         Vue.prototype.$http = axios.create({
             baseURL: 'http://127.0.0.1:3333/',
             headers:{
-                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImRhdGEiOnsidXNlcm5hbWUiOiJVc2VyIERlZmF1bHQiLCJlbWFpbCI6ImFwaUBhcGkuY29tIiwiYWNjZXNzIjoibWFuYWdlciJ9LCJpYXQiOjE1ODA0MTUxMDEsImV4cCI6MTU4MDUwMTUwMX0.imJEtZ5yJVE577LniagVzfQ-mZGfj6JFX8rkB5WXk0Y"
+                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImRhdGEiOnsidXNlcm5hbWUiOiJVc2VyIERlZmF1bHQiLCJlbWFpbCI6ImFwaUBhcGkuY29tIiwiYWNjZXNzIjoibWFuYWdlciJ9LCJpYXQiOjE1ODA1MDIxOTEsImV4cCI6MTU4MDU4ODU5MX0.dtYh62WTey9fb6wY9-_kOGdBaVGSsmPXJ-HNWS84xxk"
             }
 
         })

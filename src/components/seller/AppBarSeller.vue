763a6f6540576cfdecc9e7fc25e61757cf60fec1
fernandotanno <fernando.tanno@gmail.com>
2020-02-11 19:09:06 -0300

    Ajust components seller

---
+++ src/components/seller/AppBarSeller.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app color="blue-grey lighten-5">
+    <v-spacer></v-spacer>
+    <v-toolbar-title>DashBoard - Seller</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/seller/DashSeller.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <NavSeller />
+    <AppBarSeller />
+  </v-app>
+</template>
+
+<script>
+import NavSeller from "@/components/seller/NavSeller";
+import AppBarSeller from "@/components/seller/AppBarSeller";
+
+export default {
+  components: {
+    AppBarSeller,
+    NavSeller
+  }
+};
+</script>
+
+<style>
+</style>

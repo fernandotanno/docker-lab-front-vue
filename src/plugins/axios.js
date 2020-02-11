822130db372faa1eabf2ab38e912675efb9f1b1d
fernandotanno <fernando.tanno@gmail.com>
2020-02-11 19:11:42 -0300

    ajust axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'http://127.0.0.1:3333/'
+// axios.defaults.baseURL = 'http://127.0.0.1:3333/' 
 // axios.defaults.headers.common['Authorization'] = 'abc123'
 // axios.defaults.headers.get['Accepts'] = 'application/json'
 
@@ -11,9 +11,9 @@ Vue.use({
         //Vue.prototype.$http = axios
         Vue.prototype.$http = axios.create({
             baseURL: 'http://127.0.0.1:3333/',
-            headers:{
-                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImRhdGEiOnsidXNlcm5hbWUiOiJVc2VyIERlZmF1bHQiLCJlbWFpbCI6ImFwaUBhcGkuY29tIiwiYWNjZXNzIjoibWFuYWdlciJ9LCJpYXQiOjE1ODA1MDIxOTEsImV4cCI6MTU4MDU4ODU5MX0.dtYh62WTey9fb6wY9-_kOGdBaVGSsmPXJ-HNWS84xxk"
-            }
+            // headers:{
+            //     "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImRhdGEiOnsidXNlcm5hbWUiOiJVc2VyIERlZmF1bHQiLCJlbWFpbCI6ImFwaUBhcGkuY29tIiwiYWNjZXNzIjoibWFuYWdlciJ9LCJpYXQiOjE1ODA3NDg0MDcsImV4cCI6MTU4MDgzNDgwN30.u8XjVwyKcMu5TTqWsKmnKY19YNBiYq1P_GIOpwKisDw"
+            // }
 
         })
     }
